4206d9ef8e0b2caaca8c3a81a0833e04d646fb40
Craig Edwards <craigroe7@gmail.com>
2024-01-23 20:20:47 -0500

    health endpoint

--- src/main/kotlin/app/avocado/plugins/Serialization.kt
+++ src/main/kotlin/app/avocado/plugins/Serialization.kt
@@ -1,8 +1,11 @@
 package app.avocado.plugins
 
+import app.avocado.utils.baseUrl
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
@@ -12,5 +15,10 @@ fun Application.configureSerialization() {
             prettyPrint = true
             isLenient = true
         })
+    }
+    routing {
+        get("$baseUrl/health") {
+            call.respondText("Service running")
+        }
     }
 }
